dad50a6a97866bf5a3c168f758169296d561a7d8
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-09-10 21:04:43 +0200

    Day 2 Part 3

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCa0mjSXjgEdfRRBw4vmFHGIju1OLWyqQY",
+  authDomain: "clone-37290.firebaseapp.com",
+  databaseURL: "https://clone-37290.firebaseio.com",
+  projectId: "clone-37290",
+  storageBucket: "clone-37290.appspot.com",
+  messagingSenderId: "40290233264",
+  appId: "1:40290233264:web:113820c9144393767bc475",
+  measurementId: "G-W2M0LC0EGE",
+};
