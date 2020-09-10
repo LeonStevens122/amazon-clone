0f428bfa5861e37519cf1134d175efc0689b206a
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-09-10 21:36:22 +0200

    Started on Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Header from "./components/header/Header";
+// import Styling
+
 import "./App.css";
+
+// Import Components
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <Header /> {/* Home */}
+      <Header />
+      <Home />
     </div>
   );
 }
